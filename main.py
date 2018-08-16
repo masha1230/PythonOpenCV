3ed52c262a6ee5a83177fa6aa9dabc71bac0ef8a
masha1230 <masha1230@gmail.com>
2018-08-16 22:40:20 +0200

    clean up
    clean up unused code

--- main.py
+++ main.py
@@ -16,24 +16,3 @@ for filename in glob.glob("data\*.png"):
         #fe.TestBlobDetection(img)
         fe.TestContourFind(img)
         #fe.adjust_gamma(img, gamma=1.0)
-
-
-
-
-##thresholding
-#ret,th1 = cv2.threshold(img,100,255,cv2.THRESH_BINARY)
-#th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-#            cv2.THRESH_BINARY,13,2)
-#th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-#            cv2.THRESH_BINARY,13,2)
-#
-#titles = ['Original Image', 'Global Thresholding (v = 127)',
-#            'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-#images = [img, th1, th2, th3]
-#
-#plt.figure()
-#for i in xrange(4):
-#    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-#    plt.title(titles[i])
-#    plt.xticks([]),plt.yticks([])
-#plt.show()
